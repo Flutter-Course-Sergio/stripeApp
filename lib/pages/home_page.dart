f766f19712907d2d777143263ca347bb6ecdb4ff
Sergio Barreras <sergiobg.isc@gmail.com>
2024-10-13 02:05:49 -0700

    style: Diseño de la pantalla Home

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+
+import '../data/cards.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pagar'),
-      ),
-      body: Center(
-        child: Text('HomePage'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Pagar'),
+          actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+        ),
+        body: Stack(
+          children: [
+            Positioned(
+              width: size.width,
+              height: size.height,
+              top: size.width / 2 + 30,
+              child: PageView.builder(
+                controller: PageController(viewportFraction: 0.9),
+                physics: const BouncingScrollPhysics(),
+                itemCount: cards.length,
+                itemBuilder: (_, i) {
+                  final card = cards[i];
+
+                  return CreditCardWidget(
+                      cardNumber: card.cardNumberHidden,
+                      expiryDate: card.expiracyDate,
+                      cardHolderName: card.cardHolderName,
+                      cvvCode: card.cvv,
+                      showBackView: false,
+                      isSwipeGestureEnabled: false,
+                      onCreditCardWidgetChange: (CreditCardBrand brand) {});
+                },
+              ),
+            )
+          ],
+        ));
   }
 }
